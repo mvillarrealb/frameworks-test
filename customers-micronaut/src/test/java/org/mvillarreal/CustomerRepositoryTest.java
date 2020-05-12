cb4b6432be9699c6fe386493cb71b30cd9c22ab4
mvillarrealb <erick.slayer.m.v@gmail.com>
2020-05-12 12:40:27 -0500

    adding gatling tests

--- customers-micronaut/src/test/java/org/mvillarreal/CustomerRepositoryTest.java
+++ customers-micronaut/src/test/java/org/mvillarreal/CustomerRepositoryTest.java
@@ -54,36 +54,20 @@ public class CustomerRepositoryTest {
         assertThat(customer.getContacts()).hasSize(1);
         assertThat(customer.getAddresses()).hasSize(0);
     }
+    /*
+        @Test
+        public void findCustomers() {
+            List<Customer> customerList = customerRepository.findAll();
+            assertThat(customerList).hasSize(10);
+        }
 
-    @Test
-    /*@Sql(statements = {
-            "insert into customers (customer_id, name, last_name, identity_document, document_type) values (1, 'Corella', 'Tustin', 8018599, 'FOREIGN_DOCUMENT');",
-            "insert into customers (customer_id, name, last_name, identity_document, document_type) values (2, 'Calv', 'Jerdan', 6744761, 'PASSPORT');",
-            "insert into customers (customer_id, name, last_name, identity_document, document_type) values (3, 'Timi', 'Brett', 6743927, 'DNI');",
-            "insert into customers (customer_id, name, last_name, identity_document, document_type) values (4, 'Opal', 'Whysall', 1003889, 'DNI');",
-            "insert into customers (customer_id, name, last_name, identity_document, document_type) values (5, 'Karoly', 'Stanyland', 5293928, 'PASSPORT');",
-            "insert into customers (customer_id, name, last_name, identity_document, document_type) values (6, 'Arvin', 'Ortas', 4539259, 'FOREIGN_DOCUMENT');",
-            "insert into customers (customer_id, name, last_name, identity_document, document_type) values (7, 'Cherye', 'McMillian', 7410422, 'FOREIGN_DOCUMENT');",
-            "insert into customers (customer_id, name, last_name, identity_document, document_type) values (8, 'Carlye', 'Rockwell', 4318190, 'FOREIGN_DOCUMENT');",
-            "insert into customers (customer_id, name, last_name, identity_document, document_type) values (9, 'Reuven', 'Gilding', 7235242, 'PASSPORT');",
-            "insert into customers (customer_id, name, last_name, identity_document, document_type) values (10, 'Anatol', 'Kordovani', 7662817, 'FOREIGN_DOCUMENT');"
-    })*/
-    public void findCustomers() {
-        List<Customer> customerList = customerRepository.findAll();
-        assertThat(customerList).hasSize(10);
-    }
-
-    @Test
-    /*@Sql(statements = {
-            "insert into customers (customer_id, name, last_name, identity_document, document_type) values (1, 'Corella', 'Tustin', 8018599, 'FOREIGN_DOCUMENT');",
-            "insert into customers (customer_id, name, last_name, identity_document, document_type) values (2, 'Calv', 'Jerdan', 6744761, 'PASSPORT');"
-    })*/
-    public void findCustomer() {
-        Optional<Customer> customer = customerRepository.findById(1L);
-        assertThat(customer.isPresent()).isEqualTo(true);
-        assertThat(customer.get()).isNotNull();
-    }
-
+        @Test
+        public void findCustomer() {
+            Optional<Customer> customer = customerRepository.findById(1L);
+            assertThat(customer.isPresent()).isEqualTo(true);
+            assertThat(customer.get()).isNotNull();
+        }
+    */
     private Contact giveMeContact() {
         return Contact.builder().contactType(ContactType.EMAIL).contact("jsmiedo@gmail.com").build();
     }
---
+++ performance-tests/src/gatling/simulations/CustomerSimulation.scala
@@ -0,0 +1,72 @@
+import scala.concurrent.duration._
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class CustomerSimulation extends Simulation {
+
+  object Customers {
+
+      val findIds = csv("find_ids.csv").random
+      val customerDocuments = csv("documents.csv").random
+
+      val search = exec(
+       http("findCustomers").get("/customers").check(status.is(200))
+      )
+      .pause(1)
+      .feed(customerDocuments)
+      .exec(
+          http("createCustomer")
+            .post("/customers")
+            .body(StringBody(
+              """
+                {
+                	"name": "Lexis",
+                	"lastName": "Oliffe",
+                	"identityDocument": "${identityDocument}",
+                	"documentType": "DNI",
+                	"contacts": [{
+                		"contactType": "EMAIL",
+                		"contact": "lelfes0@prlog.org"
+                	}, {
+                		"contactType": "PHONE",
+                		"contact": "wsealand1@de.vu"
+                	}],
+                	"addresses": [{
+                		"department": "LIMA",
+                		"province": "LIMA",
+                		"district": "VILLA EL SALVADOR",
+                		"latitude": 37.933843,
+                		"longitude": 106.337072,
+                		"address": "544"
+                	}]
+                }
+              """.stripMargin))
+            .header("Content-type","application/json")
+            .asJson
+            .check(status.is(201))
+       )
+      .pause(1)
+      .feed(findIds)
+      .exec(
+        http("findOne").get("/customers/${customerId}").check(status.in(200,404))
+      )
+
+      .pause(1)
+  }
+
+  val httpProtocol = http
+    .baseUrl("http://localhost:8080")
+    .acceptHeader("application/json")
+    .acceptEncodingHeader("gzip, deflate")
+
+  val customers = scenario("Customers").exec(Customers.search)
+
+  setUp(customers.inject(
+    incrementUsersPerSec(20)
+      .times(5)
+      .eachLevelLasting(5 seconds)
+      .startingFrom(1)
+    ))
+    .protocols(httpProtocol)
+    .assertions(global.successfulRequests.percent.is(100))
+}
